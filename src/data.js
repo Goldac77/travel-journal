33cd5333f683b1035f99ea74134c4c22c836f814
Nicholas Amemazior <amemaziornicholas77@gmail.com>
2022-11-18 13:14:06 +0000

    initializing project

---
+++ src/data.js
@@ -0,0 +1,11 @@
+export default [
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNBB",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Japan’s Mt. Fuji is an active volcano about 100 kilometers southwest of Tokyo. Commonly called “Fuji-san,” it’s the country’s tallest peak, at 3,776 meters.",
+        imageUrl: "https://images.unsplash.com/photo-1578637387939-43c525550085?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+    }
+]
