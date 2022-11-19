c6fe2b1de9d47baf09f4f38acb7be332a705f578
Nicholas Amemazior <amemaziornicholas77@gmail.com>
2022-11-19 09:43:27 +0000

    completed

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-export default [
+var dataSet =  [
     {
         title: "Mount Fuji",
         location: "Japan",
@@ -9,21 +9,23 @@ export default [
         imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
     },
     {
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNBB",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Japan’s Mt. Fuji is an active volcano about 100 kilometers southwest of Tokyo. Commonly called “Fuji-san,” it’s the country’s tallest peak, at 3,776 meters.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        title: "Sydney Opera House",
+        location: "Australia",
+        googleMapsUrl: "https://shrtco.de/z1Xr6N",
+        startDate: "27 May, 2021",
+        endDate: "8 Jun, 2021",
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the foreshore of Sydney Harbour, it is widely regarded as one of the world's most famous and distinctive buildings and a masterpiece of 20th century architecture",
+        imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
     },
     {
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNBB",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Japan’s Mt. Fuji is an active volcano about 100 kilometers southwest of Tokyo. Commonly called “Fuji-san,” it’s the country’s tallest peak, at 3,776 meters.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        title: "Geirangerfjord",
+        location: "Norway",
+        googleMapsUrl: "https://shrtco.de/5p6XCg",
+        startDate: "01 Oct, 2021",
+        endDate: "18 Nov, 2021",
+        description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality",
+        imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
     }
 ]
+
+export default dataSet
